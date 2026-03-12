044fd76c8bce4cde03acd503f1b5cfe69e06ee7d
Jeremy <ggajy123@gmail.com>
2026-03-12 10:26:37 -0700

    feat: add TriggerInfo component for trigger metadata

---
+++ Assets/Scripts/Building/TriggerInfo.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Building
+{
+    public class TriggerInfo : MonoBehaviour
+    {
+        public Vector3Int GridPosition;
+        public BoardOrientation Orientation;
+    }
+}
